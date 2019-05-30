0bec4eabf1b0896b28189e837484d1670db80b49
pdroalbqrq <pdroalbqrq@gmail.com>
2019-05-30 11:18:57 -0300

    url alter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = module.exports = express();
 require("dotenv").config();
 
 app.use(express.static(__dirname + '/static/assets'))
-app.use(cors({ origin: 'http://localhost:4200'}));
+app.use(cors({ origin: process.env.URL_ORIGIN}));
 
 const clientes = require('./rotas/cliente')
 const usuarios = require('./rotas/usuario')
